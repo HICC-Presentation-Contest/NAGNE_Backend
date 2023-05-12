20278b352a0f67b3e7c8dcb6d0807955e1cd7d07
tlarbals824 <simkm0824@naver.com>
2023-05-12 15:29:39 +0900

    [chore] 프로젝트 세팅

---
+++ src/main/java/com/hicc/nagne_backend/NagneBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hicc.nagne_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NagneBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NagneBackendApplication.class, args);
+	}
+
+}
