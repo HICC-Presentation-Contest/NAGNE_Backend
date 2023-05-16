683054783298eea3095573bbe6ec7a51fffce4a1
seo-young <sy0hiu@gmail.com>
2023-05-16 16:12:12 +0900

    [feat] User파일들 entity, repository, service로 분리

--- src/main/java/com/hicc/nagne_backend/domain/user/domain/service/UserSaveService.java
+++ src/main/java/com/hicc/nagne_backend/domain/user/domain/service/UserSaveService.java
@@ -1,5 +1,7 @@
-package com.hicc.nagne_backend.domain.user.domain;
+package com.hicc.nagne_backend.domain.user.domain.service;
 
+import com.hicc.nagne_backend.domain.user.domain.entity.User;
+import com.hicc.nagne_backend.domain.user.domain.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
