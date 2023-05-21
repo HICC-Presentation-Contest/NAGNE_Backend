4b0175d25c162de64b5dfe754fa7123b67a2286e
seo-young <sy0hiu@gmail.com>
2023-05-21 20:15:35 +0900

    [feat] 팔로우 기능 추가

--- src/main/java/com/hicc/nagne_backend/domain/follow/application/service/FollowerGetService.java
+++ src/main/java/com/hicc/nagne_backend/domain/follow/application/service/FollowerGetService.java
@@ -1,6 +1,6 @@
 package com.hicc.nagne_backend.domain.follow.application.service;
 
-import com.hicc.nagne_backend.domain.follow.application.dto.FollowResponse;
+import com.hicc.nagne_backend.domain.follow.application.dto.response.FollowResponse;
 import com.hicc.nagne_backend.domain.follow.application.mapper.FollowMapper;
 import com.hicc.nagne_backend.domain.follow.domain.entity.Follow;
 import com.hicc.nagne_backend.domain.follow.domain.service.FollowerQueryService;
