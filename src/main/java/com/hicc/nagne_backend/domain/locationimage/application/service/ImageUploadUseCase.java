6b1d9709445c39ab6f349759de5784d52552f544
seo-young <sy0hiu@gmail.com>
2023-06-17 01:07:20 +0900

    [fix] LocationImage 엔티티 저장 UseCase 로직 수정

--- src/main/java/com/hicc/nagne_backend/domain/locationimage/application/service/ImageUploadUseCase.java
+++ src/main/java/com/hicc/nagne_backend/domain/locationimage/application/service/ImageUploadUseCase.java
@@ -1,13 +1,10 @@
 package com.hicc.nagne_backend.domain.locationimage.application.service;
 
 import com.hicc.nagne_backend.common.annotation.UseCase;
+import com.hicc.nagne_backend.domain.locationimage.domain.entity.LocationImage;
 import com.hicc.nagne_backend.domain.locationimage.domain.service.ImageSaveService;
-import com.hicc.nagne_backend.domain.s3.infrastructure.S3UploadService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
 
 @UseCase
 @RequiredArgsConstructor
@@ -15,19 +12,8 @@ import java.io.IOException;
 public class ImageUploadUseCase {
 
     private final ImageSaveService imageSaveService;
-    private final S3UploadService s3UploadService;
 
-    public String uploadImage(MultipartFile multipartFile) throws IOException {
-
-        /*
-        LocationImage locationImage = LocationImage.builder()
-                .imageUrl(multipartFile.getOriginalFilename())
-                .build();
-
+    public void uploadImage(LocationImage locationImage) {
         imageSaveService.saveImage(locationImage);
-        */
-
-        return s3UploadService.upload(multipartFile);
-
     }
 }
