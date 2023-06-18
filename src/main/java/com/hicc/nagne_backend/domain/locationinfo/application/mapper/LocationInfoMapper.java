c43f37d109a878efd69c1e3491c614b5298c023c
tlarbals824 <simkm0824@naver.com>
2023-06-18 21:47:49 +0900

    [fix] locationInfo에 경도 위도 정보 추가에 따른 반환값 수정

--- src/main/java/com/hicc/nagne_backend/domain/locationinfo/application/mapper/LocationInfoMapper.java
+++ src/main/java/com/hicc/nagne_backend/domain/locationinfo/application/mapper/LocationInfoMapper.java
@@ -2,6 +2,7 @@ package com.hicc.nagne_backend.domain.locationinfo.application.mapper;
 
 import com.hicc.nagne_backend.domain.locationinfo.application.dto.request.LocationInfoRequest;
 import com.hicc.nagne_backend.domain.locationinfo.application.dto.response.LocationInfoResponse;
+import com.hicc.nagne_backend.domain.locationinfo.domain.entity.Address;
 import com.hicc.nagne_backend.domain.locationinfo.domain.entity.LocationInfo;
 import com.hicc.nagne_backend.domain.trip.domain.entity.Trip;
 import lombok.AccessLevel;
@@ -17,16 +18,16 @@ public class LocationInfoMapper
     public static List<LocationInfoResponse.LocationInfoUserResponse> mapToLocationInfoUserResponse(List<LocationInfo> locationInfoList) {
         return locationInfoList.stream()
                 .map(locationInfo -> LocationInfoResponse.LocationInfoUserResponse.builder()
-                        .place(locationInfo.getAddress())
+                        .place(locationInfo.getAddress().getAddress())
                         .sequence(locationInfo.getSequence())
                         .build())
                 .collect(Collectors.toList());
     }
 
-    public static LocationInfo mapToLocationInfo(Trip trip, LocationInfoRequest.LocationInfoCreate locationInfoCreate) {
+    public static LocationInfo mapToLocationInfo(Trip trip, LocationInfoRequest.LocationInfoCreate locationInfoCreate, Address address) {
         return LocationInfo.builder()
                 .trip(trip)
-                .address(locationInfoCreate.getAddress())
+                .address(address)
                 .description(locationInfoCreate.getDescription())
                 .sequence(locationInfoCreate.getSequence())
                 .build();
