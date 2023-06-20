99690bfebb845c877729043d7e0568d5a6f12681
seo-young <sy0hiu@gmail.com>
2023-06-20 16:06:05 +0900

    [fix] 여정 slice로 반환하도록 수정

--- src/main/java/com/hicc/nagne_backend/domain/trip/application/service/TripGetUseCase.java
+++ src/main/java/com/hicc/nagne_backend/domain/trip/application/service/TripGetUseCase.java
@@ -1,16 +1,18 @@
 package com.hicc.nagne_backend.domain.trip.application.service;
 
 import com.hicc.nagne_backend.common.annotation.UseCase;
+import com.hicc.nagne_backend.common.slice.SliceResponse;
 import com.hicc.nagne_backend.domain.locationimage.domain.service.LocationImageQueryService;
 import com.hicc.nagne_backend.domain.locationinfo.application.dto.response.LocationInfoResponse;
 import com.hicc.nagne_backend.domain.locationinfo.application.mapper.LocationInfoMapper;
-import com.hicc.nagne_backend.domain.locationinfo.domain.entity.LocationInfo;
 import com.hicc.nagne_backend.domain.locationinfo.domain.service.LocationInfoQueryService;
 import com.hicc.nagne_backend.domain.trip.application.dto.response.TripResponse;
 import com.hicc.nagne_backend.domain.trip.application.mapper.TripMapper;
 import com.hicc.nagne_backend.domain.trip.domain.entity.Trip;
 import com.hicc.nagne_backend.domain.trip.domain.service.TripQueryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -37,9 +39,15 @@ public class TripGetUseCase {
         return tripInfoResponse;
     }
 
-    public List<TripResponse.TripSimpleResponse> getTripList(Long userId) {
-        List<Trip> tripList = tripQueryService.findByUserId(userId);
-        Long tripCount = tripQueryService.countByUserId(userId);
-        return TripMapper.mapToTripInfoResponseList(tripList, tripCount);
+    public SliceResponse<TripResponse.TripSimpleResponse> getTripList(Long userId, Pageable pageable) {
+        Slice<Trip> tripList = tripQueryService.findByUserId(userId, pageable);
+
+        Slice<TripResponse.TripSimpleResponse> TripSimpleResponseList =
+                tripList.map(trip ->
+                {
+                    return TripMapper.mapToTripSimpleResponse(trip);
+                });
+
+        return SliceResponse.of(TripSimpleResponseList);
     }
 }
