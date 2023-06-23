768c8c8b5722cd44ea9e26187e90f28a1f2aeaf7
seo-young <sy0hiu@gmail.com>
2023-06-23 13:08:32 +0900

    [feat] 첫화면 생성에 필요한 LocationInfo dto 추가, 스웨거 @Schema 추가

--- src/main/java/com/hicc/nagne_backend/domain/locationinfo/application/dto/request/LocationInfoRequest.java
+++ src/main/java/com/hicc/nagne_backend/domain/locationinfo/application/dto/request/LocationInfoRequest.java
@@ -1,6 +1,7 @@
 package com.hicc.nagne_backend.domain.locationinfo.application.dto.request;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,9 +15,13 @@ public class LocationInfoRequest {
     @NoArgsConstructor
     public static class LocationInfoCreate {
 
-        private String address; //위치
-        private String description; //설명
-        private String sequence; //순서
+        @Schema(description = "장소", defaultValue = "address")
+        private String address;
+        @Schema(description = "설명", defaultValue = "description")
+        private String description;
+        @Schema(description = "순서", defaultValue = "sequence")
+        private String sequence;
+        @Schema(description = "이미지 리스트", defaultValue = "locationImage")
         private MultipartFile locationImage;
 
         @Builder
--- src/main/java/com/hicc/nagne_backend/domain/locationinfo/application/dto/response/LocationInfoResponse.java
+++ src/main/java/com/hicc/nagne_backend/domain/locationinfo/application/dto/response/LocationInfoResponse.java
@@ -8,11 +8,27 @@ import lombok.NoArgsConstructor;
 
 public class LocationInfoResponse {
 
+    @Getter
+    @NoArgsConstructor
+    public static class LocationInfoMainPageResponse {
+        @Schema(description = "위치 정보", defaultValue = "address")
+        private Address address;
+        @Schema(description = "순서", defaultValue = "sequence")
+        private String sequence;
+
+        @Builder
+        public LocationInfoMainPageResponse(Address address, String sequence) {
+            this.address = address;
+            this.sequence = sequence;
+        }
+    }
 
     @Getter
     @NoArgsConstructor
     public static class LocationInfoSimpleResponse {
+        @Schema(description = "장소 이름", defaultValue = "placeName")
         private String placeName;
+        @Schema(description = "순서", defaultValue = "sequence")
         private String sequence;
 
         @Builder
@@ -24,9 +40,13 @@ public class LocationInfoResponse {
 
     @Getter
     public static class LocationInfoDetailsResponse{
+        @Schema(description = "장소 정보", defaultValue = "place")
         private String place;
+        @Schema(description = "설명", defaultValue = "description")
         private String description;
+        @Schema(description = "순서", defaultValue = "sequence")
         private String sequence;
+        @Schema(description = "이미지", defaultValue = "imageUrl")
         private String imageUrl;
 
         @Builder
@@ -40,9 +60,9 @@ public class LocationInfoResponse {
 
     @Getter
     public static class LocationInfoBookMarkResponse{
-        @Schema(description = "장소 정보")
+        @Schema(description = "장소 정보", defaultValue = "address")
         private Address address;
-        @Schema(description = "여행 순서")
+        @Schema(description = "여행 순서", defaultValue = "sequence")
         private String sequence;
 
         @Builder
