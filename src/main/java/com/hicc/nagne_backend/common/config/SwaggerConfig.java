de014a6856407e3dee52193f53b132d6e30b6306
tlarbals824 <simkm0824@naver.com>
2023-06-23 11:40:02 +0900

    [feat] 스웨거 설정에 user, follow, bookmark 페이지 추가

--- src/main/java/com/hicc/nagne_backend/common/config/SwaggerConfig.java
+++ src/main/java/com/hicc/nagne_backend/common/config/SwaggerConfig.java
@@ -1,13 +1,29 @@
 package com.hicc.nagne_backend.common.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import lombok.RequiredArgsConstructor;
 import org.springdoc.core.GroupedOpenApi;
 import org.springdoc.core.customizers.OpenApiCustomiser;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Nagne API",
+                description = "Nagne API 문서입니다.",
+                version = "v1"),
+        servers = {
+                @Server(url = "http://3.37.189.80", description = "운영서버"),
+                @Server(url = "http://localhost:8080", description = "로컬서버")
+        }
+
+)
 @Configuration
+@RequiredArgsConstructor
 public class SwaggerConfig {
 
     @Bean
@@ -15,6 +31,36 @@ public class SwaggerConfig {
         String[] paths = {"/trip/**"};
         return GroupedOpenApi.builder()
                 .group("Trip API")
+                .pathsToMatch(paths)
+                .addOpenApiCustomiser(buildSecurityOpenApi())
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi userOpenApi() {
+        String[] paths = {"/user/**"};
+        return GroupedOpenApi.builder()
+                .group("User API")
+                .pathsToMatch(paths)
+                .addOpenApiCustomiser(buildSecurityOpenApi())
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi bookmarkOpenApi() {
+        String[] paths = {"/bookmark/**"};
+        return GroupedOpenApi.builder()
+                .group("Bookmark API")
+                .pathsToMatch(paths)
+                .addOpenApiCustomiser(buildSecurityOpenApi())
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi followOpenApi() {
+        String[] paths = {"/follow/**"};
+        return GroupedOpenApi.builder()
+                .group("Follow API")
                 .pathsToMatch(paths)
                 .addOpenApiCustomiser(buildSecurityOpenApi())
                 .build();
